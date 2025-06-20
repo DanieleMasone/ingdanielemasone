4a01808bf56d4cd7d85e5e2eecc9eeb3c20bd000
daniele.masone <masone.daniele@rgigroup.com>
2025-06-20 16:49:18 +0200

    Deploy: app + docs + coverage in single branch

---
+++ docs/entry.js
@@ -0,0 +1,62 @@
+
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import React from "react";
+
+      import ReactDOM from "react-dom";
+
+
+      import ReactWrapper from '../node_modules/better-docs/lib/react-wrapper.js';
+
+      window.React = React;
+
+      window.ReactDOM = ReactDOM;
+
+      window.ReactWrapper = ReactWrapper;
+
+    
+    import './styles/reset.css';
+
+    import './styles/iframe.css';
+
+  import Component0 from '../src/pages/CookieBanner.jsx';
+reactComponents['CookieBanner'] = Component0;
+
+import Component1 from '../src/components/ui/DarkModeToggle.jsx';
+reactComponents['DarkModeToggle'] = Component1;
+
+import Component2 from '../src/components/ui/Footer.jsx';
+reactComponents['Footer'] = Component2;
+
+import Component3 from '../src/components/ui/LanguageSwitcher.jsx';
+reactComponents['LanguageSwitcher'] = Component3;
+
+import Component4 from '../src/components/ui/TradingPerformanceChart.jsx';
+reactComponents['TradingPerformanceChart'] = Component4;
+
+import Component5 from '../src/pages/CookiePolicy.jsx';
+reactComponents['CookiePolicy'] = Component5;
+
+import Component6 from '../src/pages/Courses.jsx';
+reactComponents['Courses'] = Component6;
+
+import Component7 from '../src/pages/Experience.jsx';
+reactComponents['Experience'] = Component7;
+
+import Component8 from '../src/pages/Home.jsx';
+reactComponents['Home'] = Component8;
+
+import Component9 from '../src/pages/Privacy.jsx';
+reactComponents['Privacy'] = Component9;
+
+import Component10 from '../src/pages/Projects.jsx';
+reactComponents['Projects'] = Component10;
+
+import Component11 from '../src/pages/Testimonials.jsx';
+reactComponents['Testimonials'] = Component11;
+
+import Component12 from '../src/pages/Trading.jsx';
+reactComponents['Trading'] = Component12;
