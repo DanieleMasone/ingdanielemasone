f044ce4faa37e194027bafdbf3ba386f820f1ea0
daniele.masone <masone.daniele@rgigroup.com>
2025-07-02 11:23:00 +0200

    chore: seo

--- src/index.js
+++ src/index.js
@@ -5,20 +5,8 @@ import './index.css';
 import './i18n/i18n';
 import i18n from "i18next";
 import {I18nextProvider} from "react-i18next";
-import {createHashRouter, RouterProvider} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
-
-const router = createHashRouter([
-    {
-        path: "/*",
-        element: <App/>,
-    },
-], {
-    future: {
-        v7_startTransition: true,
-        v7_relativeSplatPath: true
-    }
-});
 
 
 i18n.init({
@@ -30,7 +18,9 @@ i18n.init({
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <I18nextProvider i18n={i18n}>
-            <RouterProvider router={router}/>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </I18nextProvider>
     </React.StrictMode>
 );
