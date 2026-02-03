0aa1cc2db74326d11085ceadf3526952196c90e9
daniele masone <masone.daniele@gmail.com>
2026-02-03 10:41:55 +0100

    Deploy: app + docs + coverage in single branch

---
+++ assets/SelectableButton-CT99mUlM.js
@@ -0,0 +1,3 @@
+import{j as a}from"./index-BB0Pzf1S.js";function b({label:t,isSelected:e,onClick:r}){return a.jsx("button",{onClick:r,className:`px-4 py-2 rounded-lg border text-sm whitespace-nowrap
+                ${e?"bg-blue-600 text-white dark:bg-blue-500":"bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200"}`,children:t})}export{b as S};
+//# sourceMappingURL=SelectableButton-CT99mUlM.js.map
