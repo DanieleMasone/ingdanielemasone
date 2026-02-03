15bb8981bf42474dafa31b9795cd38c2b94785dc
daniele masone <masone.daniele@gmail.com>
2026-02-03 10:06:08 +0100

    fix: Domain

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [react()],
+    base: '/ingdanielemasone/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
