0d03048562e7ee779418b79f06a19a0c01c6caf2
daniele masone <masone.daniele@gmail.com>
2026-02-06 15:59:24 +0100

    Deploy: app + docs + coverage in single branch

---
+++ assets/CardContent-CqIRALzb.js
@@ -0,0 +1,7 @@
+import{j as e}from"./index-D_xIfntT.js";function m({children:t,className:r="",...s}){return e.jsx("div",{...s,className:`
+                p-4 sm:p-6 md:p-8
+                text-sm sm:text-base
+                text-gray-900 dark:text-gray-300
+                ${r}
+            `.trim(),children:t})}export{m as C};
+//# sourceMappingURL=CardContent-CqIRALzb.js.map
