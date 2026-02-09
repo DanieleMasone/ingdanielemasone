da8839173ce6e93c1eef7970395fb6d49253fda2
daniele masone <masone.daniele@gmail.com>
2026-02-09 12:12:51 +0100

    Deploy: app + docs + coverage in single branch

---
+++ assets/Card-P3N8zWNN.js
@@ -0,0 +1,7 @@
+import{j as s}from"./index-DgvQkAhs.js";function e({children:r,className:a="",...o}){return s.jsx("section",{...o,className:`
+                rounded-2xl bg-gray-100 dark:bg-gray-800 
+                border border-gray-300 dark:border-gray-700 
+                shadow-md hover:shadow-lg transition-shadow 
+                p-4 sm:p-6 w-full ${a}
+            `.trim(),children:r})}export{e as C};
+//# sourceMappingURL=Card-P3N8zWNN.js.map
