febb4eeb9d153815e35944fe4f9344c95f0f7f52
daniele masone <masone.daniele@gmail.com>
2026-02-17 10:46:26 +0100

    chore: Add translations

--- src/mock/certifications.js
+++ src/mock/certifications.js
@@ -5,18 +5,21 @@ export const certifications = [
         nameKey: "English CEFR Level B2.2",
         issuer: "Speexx",
         date: "2024",
+        descriptionKey: "Speexx_B2_2",
         link: "https://portal.speexx.com/certificate/ZTAxY2Y5ZTktNGRjMi00Yzc0LWFjYmQtZjAzOTIyZDNmZjdmOjo4Mg=="
     },
     {
         nameKey: "English CEFR Level B2.1",
         issuer: "Speexx",
         date: "2024",
+        descriptionKey: "Speexx_B2_1",
         link: "https://portal.speexx.com/certificate/ZTAxY2Y5ZTktNGRjMi00Yzc0LWFjYmQtZjAzOTIyZDNmZjdmOjo4MQ=="
     },
     {
         nameKey: "English CEFR Level B1.2",
         issuer: "Speexx",
         date: "2024",
+        descriptionKey: "Speexx_B1_2",
         link: "https://portal.speexx.com/certificate/ZTAxY2Y5ZTktNGRjMi00Yzc0LWFjYmQtZjAzOTIyZDNmZjdmOjo4MA=="
     },
     {
@@ -30,18 +33,21 @@ export const certifications = [
         nameKey: "English CEFR Level B1.1",
         issuer: "Speexx",
         date: "2023",
+        descriptionKey: "Speexx_B1_1",
         link: "https://portal.speexx.com/certificate/ZTAxY2Y5ZTktNGRjMi00Yzc0LWFjYmQtZjAzOTIyZDNmZjdmOjo3OQ=="
     },
     {
         nameKey: "Codemotion Milan",
         issuer: "Codemotion",
         date: "2023",
+        descriptionKey: "Codemotion_Milan",
         link: "https://credsverse.com/credentials/070cb7c9-c2c8-4bf5-b27b-df66298f9561",
     },
     {
         nameKey: "Codemotion Workshop Fest",
         issuer: "Codemotion",
         date: "2023",
+        descriptionKey: "Codemotion_Workshop",
         link: "https://credsverse.com/credentials/98c00e0e-8cb8-41d1-96b0-515b1b2842ef",
     }
 ];
