d10f0fdc552bde2e8d6f65ba302e29028baec6b8
daniele masone <masone.daniele@gmail.com>
2026-02-18 14:59:06 +0100

    Deploy: app + docs + coverage in single branch

---
+++ assets/CardContent-BotosIB3.js
@@ -0,0 +1,18 @@
+import{j as a}from"./index-C4Q_RvWG.js";function t({children:r,className:e="",...s}){return a.jsx("section",{...s,className:`
+                flex flex-col
+                rounded-3xl
+                bg-white/60 dark:bg-gray-800/60
+                backdrop-blur-md
+                border border-gray-200 dark:border-gray-700
+                shadow-md hover:shadow-xl transition-shadow duration-300
+                p-4 sm:p-6
+                w-full
+                focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500/70
+                ${e}
+            `,children:r})}function d({children:r,className:e="",...s}){return a.jsx("div",{...s,className:`
+                p-4 sm:p-6 md:p-8
+                text-sm sm:text-base
+                text-gray-900 dark:text-gray-300
+                ${e}
+            `.trim(),children:r})}export{t as C,d as a};
+//# sourceMappingURL=CardContent-BotosIB3.js.map
